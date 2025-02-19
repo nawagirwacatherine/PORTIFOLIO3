da999f4eef1b778520052012e2d4f36e683db768
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-02-19 03:49:02 +0200

    Add image to Hero com[ponent

--- real portifolio/src/components/Hero/Hero.jsx
+++ real portifolio/src/components/Hero/Hero.jsx
@@ -5,7 +5,8 @@ import restaurant1 from '../../images/restaurant1.jpeg'
 const Hero = () => {
   return (
     <div className='hero'>
-        <img src={restaurant1} alt="" />
+        <img src={restaurant1} alt="" className='logo' />
+        <h1>Im Nawagirwa Catherine, frontend developer based in Uganda</h1>
     </div>
   )
 }

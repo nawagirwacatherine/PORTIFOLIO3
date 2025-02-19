4d3f8972594dfa5ff797a2aa3ffb2d39f8eb4945
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-02-19 04:00:00 +0200

    Add more 2 divisions in the hero component

--- real portifolio/src/components/Hero/Hero.jsx
+++ real portifolio/src/components/Hero/Hero.jsx
@@ -7,6 +7,19 @@ const Hero = () => {
     <div className='hero'>
         <img src={restaurant1} alt="" className='logo' />
         <h1>Im Nawagirwa Catherine, frontend developer based in Uganda</h1>
+        <p>Iam a frontend developer from kampala uganda,With 2years of exprience. </p>
+
+        <div  className='hero-action'>
+
+        <div  className='hero-connect'>
+            Connect with me
+            </div>
+
+            <div  className='hero-resume'>
+            My resume
+            </div>
+
+        </div>
     </div>
   )
 }

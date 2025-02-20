0c7cbc10e9387c9ba7254ce4c121408b81ff911f
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-02-20 13:13:23 +0200

    add service component

--- real portifolio/src/App.jsx
+++ real portifolio/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
+import Services from './components/Services/Services'
 
 const App = ()  => {
 
@@ -13,6 +14,7 @@ const App = ()  => {
       <Navbar />
       <Hero />
       <About />
+      <Services />
     </div>
   )
 }
---
+++ real portifolio/src/components/services/Services.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Services.css'
+
+const Service = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Service

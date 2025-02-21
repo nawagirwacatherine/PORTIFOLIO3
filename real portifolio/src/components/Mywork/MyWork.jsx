8dbb9f8867776df17aba35f1ec5a31479c8aef28
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-02-21 08:11:56 +0200

    Add styling to my work component

--- real portifolio/src/components/Mywork/MyWork.jsx
+++ real portifolio/src/components/Mywork/MyWork.jsx
@@ -1,19 +1,24 @@
 import './mywork.css'
 import project from '../../images/project.jpg'
 import myWorkData from '../../images/myWorkData'
+import { FaArrowRight } from "react-icons/fa";
 
 const MyWork = () => {
   return (
     <div className='mywork'>
        <div className="mywork-title">
         <h1>My latest work</h1>
-        <img src={project} alt="" />
+        <img src={project} alt="" className='logo' />
        </div>
        <div className="mywork-container">
         {myWorkData.map((work,index) =>{
-        return <img key={index} src={work.wImg} alt="" />
+        return <img key={index} src={work.wImg} alt="" className='logo' />
         })}
        </div>
+       <div>
+        <p>Show More</p>
+       </div>
+       <FaArrowRight />
     </div>
   )
 }

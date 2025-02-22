c178f9fea574c8687db935d86b3f5ccaab001895
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-02-22 05:26:30 +0200

    add menu and closing icon

--- real portifolio/src/components/Navbar/Navbar.jsx
+++ real portifolio/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,8 @@
 import React, { useState } from 'react';
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import '../Navbar.css'
-
+import { TfiMenu } from "react-icons/tfi";
+import { RiCloseLine } from "react-icons/ri";
 import restaurant1 from '../../images/restaurant1.jpeg'
 import restaurant3 from '../../images/restaurant3.jpg'
 
@@ -12,7 +13,9 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         < img src={restaurant1} alt="" className='logo'/>
+        <TfiMenu />
         <ul className='nav-menu'>
+        <RiCloseLine />
             <li><AnchorLink className='anchor-link'  href='#home'><p onClick={() =>setMenu("home")}>Home</p></AnchorLink>{menu==="home"?<img src={restaurant3}  alt=''  className='logo'/>:<></>} </li>
             <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={() =>setMenu("about")}>About Me</p></AnchorLink>{menu==="about"?<img src={restaurant3}  alt='' className='logo'/>:<></>}</li>
             <li><AnchorLink className='anchor-link' offset={50} href='#services'><p  onClick={() =>setMenu("services")}>Services</p></AnchorLink>{menu==="services"?<img src={restaurant3}  alt='' className='logo'/>:<></>}</li>

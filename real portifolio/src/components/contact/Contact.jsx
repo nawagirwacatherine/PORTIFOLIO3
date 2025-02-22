0475101bff2d94a0c964a31144ba70bc6ac963bc
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-02-22 05:01:02 +0200

    Added web3 functionality to the form

--- real portifolio/src/components/contact/Contact.jsx
+++ real portifolio/src/components/contact/Contact.jsx
@@ -5,6 +5,29 @@ import { MdAddIcCall } from "react-icons/md";
 import { CiLocationOn } from "react-icons/ci";
 
 const Contact = () => {
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "c41fa709-45d2-4280-a5a3-36887c5de615");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          alert(res.message);
+        }
+      };
   return (
     <div id='contact' className='contact'>
         <div className="contact-title">
@@ -31,7 +54,7 @@ const Contact = () => {
                 </div>
             </div>
 
-            <form  className="contact-right">
+            <form onSubmit={onSubmit} className="contact-right">
                 <label htmlFor="">Your Name</label>
                 <input type="text" placeholder='Enter your name' name='name' />
                 <label htmlFor="">Your Email</label>
